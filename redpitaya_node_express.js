82d6a7a5ae9dc2ecf5223e3e088a9d4bc9387df0
Luis Villaseñor <lvillasen@gmail.com>
2023-05-06 00:04:58 -0600

    Update redpitaya_node_express.js

--- redpitaya_node_express.js
+++ redpitaya_node_express.js
@@ -1,19 +1,20 @@
 const express   = require('express');
 const execSync = require('child_process').execSync;
-const app       = express();
+const app    = express();
 const cors   = require('cors');
+
 app.use(cors({origin:"*",}))
 
 app.get('/leds/:id', function (req, res) {
-const output = execSync('monitor 0x40000030 '+req.params.id, { encoding: 'utf-8' });  
-console.log('LEDs changed to '+ req.params.id);
-  res.send('LEDs changed to '+ req.params.id);
-}); 
+    const output = execSync('monitor 0x40000030 '+req.params.id, { encoding: 'utf-8' });
+    console.log('LEDs changed to '+ req.params.id);
+    res.send('LEDs changed to '+ req.params.id);
+});
 
 app.get('/leds/', function (req, res) {
-const output = execSync('monitor 0x40000030', { encoding: 'utf-8' });  
-console.log('LEDs status is ' + output);
-  res.send('LEDs status is ' + output);
+    const output = execSync('monitor 0x40000030', { encoding: 'utf-8' });
+    console.log('LEDs status is ' + output);
+    res.send('LEDs status is ' + output);
 });
 
 app.listen(3000, () => {
